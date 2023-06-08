e0377857a01cdaf91fe8ec20a9a7c5429180859c
mastercodeon314 <78676320+mastercodeon314@users.noreply.github.com>
2023-06-07 18:54:54 -0600

    Update TestOut AutoLogin.user.js
    Update v1.1!
    Script will now detect when a course video is being played, and will move to the next section in the course once the video is done playing automatically!

--- TestOut AutoLogin.user.js
+++ TestOut AutoLogin.user.js
@@ -15,6 +15,30 @@
 
 (async function() {
     'use strict';
+    function delay(time) {
+        return new Promise(resolve => setTimeout(resolve, time));
+    }
+    (() => {
+        let oldPushState = history.pushState;
+        history.pushState = function pushState() {
+            let ret = oldPushState.apply(this, arguments);
+            window.dispatchEvent(new Event('pushstate'));
+            window.dispatchEvent(new Event('locationchange'));
+            return ret;
+        };
+
+        let oldReplaceState = history.replaceState;
+        history.replaceState = function replaceState() {
+            let ret = oldReplaceState.apply(this, arguments);
+            window.dispatchEvent(new Event('replacestate'));
+            window.dispatchEvent(new Event('locationchange'));
+            return ret;
+        };
+
+        window.addEventListener('popstate', () => {
+            window.dispatchEvent(new Event('locationchange'));
+        });
+    })();
 
     var encKey = GM_getValue ("encKey", "");
     var usr = GM_getValue ("lognUsr", "");
@@ -62,10 +86,67 @@
         GM_setValue (setValVarName, encryptAndStore (targVar) );
     }
 
-    window.addEventListener('load', async function() {
-        function delay(time) {
-            return new Promise(resolve => setTimeout(resolve, time));
+    window.addEventListener('locationchange', async function () {
+        await delay(350);
+
+        var nextBtn = null;
+        var aS = document.getElementsByTagName("a");
+        //console.log(aS);
+        if (aS.length > 0)
+        {
+            for (var i = 0; i < aS.length; i++)
+            {
+
+                if (aS[i].className == "to-router-button-link secondary-text ProductViewer-nav-button" && aS[i].id == "ProductViewer-NavNextBtn")
+                {
+                    nextBtn = aS[i];
+                    break;
+                }
+            }
         }
+
+
+        var vido = document.getElementsByTagName("button");
+        //console.log(vido);
+        var foundVideoPlayer = false;
+        var videoPlayerTag = null;
+        if (vido.length > 0)
+        {
+            for (var xx = 0; xx < vido.length; xx++)
+            {
+                //console.log(vido[xx].class);
+                if (vido[xx].className == "to-button primary")
+                {
+                    foundVideoPlayer = true;
+                    videoPlayerTag = vido[xx];
+                    break;
+               }
+            }
+        }
+
+        if (foundVideoPlayer == true)
+        {
+            videoPlayerTag.addEventListener('click', async function () {
+                console.log("Clicked play video button!");
+
+                await delay(350);
+
+                var divs = document.getElementsByTagName("video");
+                //console.log(divs);
+                if (divs.length > 0)
+                {
+                    divs[0].addEventListener("ended", function () {
+                        console.log("Video ended!");
+                        nextBtn.click();
+                    });
+                }
+            });
+        }
+    });
+
+    window.addEventListener('load', async function() {
+        console.log("Page Loaded!");
+
         if (!encKey)
         {
             encKey = prompt (
@@ -114,7 +195,7 @@
                 var buttons = document.getElementsByTagName("button");
                 buttons[1].click();
 
-                await delay(1500);
+                await delay(2000);
 
                 buttons = document.getElementsByTagName("button");
                 //console.log(buttons);
@@ -124,7 +205,7 @@
                     if (buttons[i].innerText == "TestOut PC Pro")
                     {
                         buttons[i].click();
-                        console.log(buttons[i]);
+                        //console.log(buttons[i]);
                         foundBtn = true;
                         break;
                     }
